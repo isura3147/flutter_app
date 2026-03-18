5b2c44a2af9c94610056e9ca2c3681488de305b6
isura <isura3147@gmail.com>
2026-03-18 16:34:04 +0530

    Add many products to menupage

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -7,8 +7,21 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Product p = Product(id: 1, name: "Coffee", price: 200, image: "");
+    Product q = Product(id: 1, name: "Black Coffee", price: 200, image: "");
+    Product t = Product(
+      id: 1,
+      name: "Another Black Coffee",
+      price: 200,
+      image: "",
+    );
 
-    return ProductItem(product: p);
+    return ListView(
+      children: [
+        ProductItem(product: p),
+        ProductItem(product: q),
+        ProductItem(product: t),
+      ],
+    );
   }
 }
 
@@ -27,19 +40,34 @@ class ProductItem extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.asset("images/black_coffee.png"),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                product.name,
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "\$${product.price}",
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        product.name,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "\$${product.price}",
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown[400],
+                  ),
+                  child: Text("Add", style: TextStyle(color: Colors.white)),
+                ),
+              ],
             ),
           ],
         ),
