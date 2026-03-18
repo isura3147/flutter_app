bc8d6b121c86bb81c9d7a4319c91eba44f9bdf20
isura <isura3147@gmail.com>
2026-03-18 15:35:09 +0530

    Add product to menupage

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,3 +1,4 @@
+import 'package:coffemasters_flutter/datamodel.dart';
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
@@ -5,6 +6,31 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Product p = Product(id: 1, name: "Coffee", price: 200, image: "");
+
+    return ProductItem(product: p);
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 10,
+        child: Column(
+          children: [
+            Image.asset("images/black_coffee.png"),
+            Text(product.name),
+            Text("\$${product.price}"),
+          ],
+        ),
+      ),
+    );
   }
 }
